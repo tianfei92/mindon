291d2e625147189bd14a1fc60689135ff22a1558
arieltian <tianfeib@zbj.com>
2018-05-09 22:11:38 +0800

    案例详情页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
+import caseDetail from '@/components/case/case'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/case/detail',
+      name: 'caseDetail',
+      component: caseDetail
     }
   ]
 })
