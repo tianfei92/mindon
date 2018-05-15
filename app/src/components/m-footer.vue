f42166ac6dfc3c9ff7bdea7d6a7fb8175f9ab0e1
arieltian <tianfeib@zbj.com>
2018-05-15 23:05:28 +0800

    图片压缩

--- app/src/components/m-footer.vue
+++ app/src/components/m-footer.vue
@@ -9,8 +9,8 @@
             <div class="right">
                 <h3>联系方式</h3>
                 <p class="tel-num">客服电话：180-0809-1258</p>
-                <p class="email">商务合作：<a href="mailto:1137333869@qq.com">1137333869@qq.com</a></p>
-                <p class="address">地址：重庆市渝北区两江新区互联网产业园</p>
+                <p class="email">商务合作：<a href="mailto:286535774@qq.com">286535774@qq.com</a></p>
+                <p class="address">地址：成都市光华村街2号天祥广场1栋2单元1705</p>
                 <p>服务时间：周一至周日 10:00-19:00</p>
             </div>
         </div>
