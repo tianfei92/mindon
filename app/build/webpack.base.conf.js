feb36cee2277d1a6899c0389f850b6f85ac13a8d
arieltian <tianfeib@zbj.com>
2018-05-22 15:21:00 +0800

    es6转码

--- app/build/webpack.base.conf.js
+++ app/build/webpack.base.conf.js
@@ -41,7 +41,6 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        query: {presets: ['es2105']},
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')],
         exclude: /node_modules/
       },
