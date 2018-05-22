e18e188aebed22918e518aff9f85db9c9ddea6ba
arieltian <tianfeib@zbj.com>
2018-05-22 16:21:30 +0800

    es6转码

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -7,7 +8,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 import $ from 'jquery'
 import mock from './plugins/mock'
-import 'babel-polyfill'
+import es6Promise from 'es6-promise'
+es6Promise.polyfill()
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
